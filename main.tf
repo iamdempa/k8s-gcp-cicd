ae4ddfff10638e4adf6cd9a3ea70cd03e855bc3c
iamdempa <banukajananathjayarathna@gmail.com>
2020-07-21 20:10:13 +0530

    test

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ resource "google_compute_subnetwork" "minions-sub" {
 resource "google_compute_firewall" "kube-master-firewall" {
   name    = "kube-master-firewall"
   network = "${google_compute_network.kubernetes-vpc.name}"
+
   allow {
     protocol = "all"
   }
